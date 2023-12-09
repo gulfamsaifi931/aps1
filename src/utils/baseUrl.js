383f616348f2c3051f64f7e4e7a2e4b7fbf8e4e4
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-09 11:49:33 +0530

    it is the first commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://alzofoods.com": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://aplusstudio.in": "http://localhost:3000"
 export default baseUrl
